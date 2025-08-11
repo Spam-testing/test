632fb58541ba0888b5153afa46193f9d5d3c39a9
Spam-testing <spammailqwerty40@gmail.com>
2025-08-11 12:35:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+quotes=["hi","test","random"]
+quote_of_the_day=random.choice(quotes)
+print(quote_of_the_day)
+quotes.remove(quote_of_the_day)
